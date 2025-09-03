31647be3026f628af17c4d5206ec9a7d7e363092
unknown <kanmanipriyas.22cse@kongu.edu>
2025-09-03 10:17:55 +0530

    commit on url change

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173', // frontend origin
+    origin: 'https://smartcart-frontend-kwrw.onrender.com/', // frontend origin
     credentials: true,
   })
 );
